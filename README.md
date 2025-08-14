18ecf42712cfcbd9adc76a9058a81ba93cd3c8ed
Adlyy03 <adlimuhamad358@gmail.com>
2025-08-14 18:28:27 +0700

    First commit - portfolio

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Portfolio
+
+Ini adalah website portofolio saya yang dibuat dengan React/Next.js.
+Didesain dan dikembangkan oleh saya sendiri untuk menampilkan proyek, artikel, dan pengalaman saya.
+
+🌐 Live demo: [Klik di sini](https://adlyy03.github.io/myportofolio/)
---
+++ data/projects.ts
@@ -0,0 +1,18 @@
+export interface Project {
+  title: string;
+  description: string;
+  logo: string;
+  link: string;
+  slug: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: 'rihaddd',
+    description:
+      'cinta rihaddd',
+    logo: '/logos/driwwwle.svg',
+    link: 'https://adlyy03.github.io/testrihaddd/',
+    slug: 'driwwwle',
+  },
+];
